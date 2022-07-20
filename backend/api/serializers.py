78aa9bde6ddc7504220a29f2dd8ab2c730a39319
Анатолий Кузин <kuzin.95@list.ru>
2022-07-20 19:44:15 +0300

    mk6

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -8,7 +8,7 @@ from rest_framework import serializers
 from recipes.models import Ingredient, Recipe, RecipeIngredient, Subscribe, Tag
 
 User = get_user_model()
-ERR_MSG = 'Не удается войти в систему с предоставленными учетными данными.'
+ERROR_MSG = 'Не удается войти в систему с предоставленными учетными данными.'
 
 
 class TokenSerializer(serializers.Serializer):
@@ -34,7 +34,7 @@ class TokenSerializer(serializers.Serializer):
                 password=password)
             if not user:
                 raise serializers.ValidationError(
-                    ERR_MSG,
+                    ERROR_MSG,
                     code='authorization')
         else:
             msg = 'Необходимо указать адрес электронной почты и пароль.'
@@ -91,7 +91,7 @@ class UserPasswordSerializer(serializers.Serializer):
                 username=user.email,
                 password=current_password):
             raise serializers.ValidationError(
-                ERR_MSG, code='authorization')
+                ERROR_MSG, code='authorization')
         return current_password
 
     def validate_new_password(self, new_password):
